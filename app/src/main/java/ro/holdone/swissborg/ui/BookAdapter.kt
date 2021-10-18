406c1ae3e05d39bd4ffeddff1a8216ab8de34a5b
Cristian Holdunu <cristian.holdunu@gmail.com>
2021-10-18 17:36:35 +0300

    basic rendering of the books

---
+++ app/src/main/java/ro/holdone/swissborg/ui/BookAdapter.kt
@@ -0,0 +1,85 @@
+package ro.holdone.swissborg.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ro.holdone.swissborg.R
+import ro.holdone.swissborg.server.model.BookEntry
+
+sealed class BookItem(val entry: BookEntry) {
+    class Ask(entry: BookEntry) : BookItem(entry)
+    class Bid(entry: BookEntry) : BookItem(entry)
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<BookItem>() {
+            override fun areItemsTheSame(oldItem: BookItem, newItem: BookItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: BookItem, newItem: BookItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class BookItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(bookEntry: BookEntry) {
+        itemView.findViewById<TextView>(R.id.volume).text = bookEntry.amount.toString()
+        itemView.findViewById<TextView>(R.id.price).text = bookEntry.price.toString()
+    }
+
+    fun cleanUp() {
+
+    }
+}
+
+class BookAdapter : ListAdapter<BookItem, BookItemViewHolder>(BookItem.diffUtil) {
+
+    lateinit var layoutInflater: LayoutInflater
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        layoutInflater = LayoutInflater.from(recyclerView.context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
+        return when (viewType) {
+            VIEW_BID -> BookItemViewHolder(
+                layoutInflater.inflate(
+                    R.layout.bid_entry,
+                    parent,
+                    false
+                )
+            )
+            VIEW_ASK -> BookItemViewHolder(
+                layoutInflater.inflate(
+                    R.layout.ask_entry,
+                    parent,
+                    false
+                )
+            )
+            else -> throw Error("Unsupported viewType=$viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
+        holder.bind(getItem(position).entry)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is BookItem.Ask -> VIEW_ASK
+            is BookItem.Bid -> VIEW_BID
+        }
+    }
+
+    companion object {
+        const val VIEW_ASK = 100
+        const val VIEW_BID = 101
+    }
+}
--- app/src/main/java/ro/holdone/swissborg/ui/TickerFragment.kt
+++ app/src/main/java/ro/holdone/swissborg/ui/TickerFragment.kt
@@ -6,16 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ro.holdone.swissborg.databinding.FragmentTickerBinding
 import ro.holdone.swissborg.server.model.CoinsPair
 
 @AndroidEntryPoint
-class TickerFragment: Fragment() {
+class TickerFragment : Fragment() {
 
     lateinit var binding: FragmentTickerBinding
 
     private val tickerViewModel: TickerViewModel by viewModels()
+
+    private val askAdapter = BookAdapter()
+    private val bidAdapter = BookAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +38,31 @@ class TickerFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tickerViewModel.tickerSnapshot.observe(viewLifecycleOwner) { snapshot->
+        setupTicker()
+        setupBooks()
+    }
+
+    private fun setupTicker() {
+        tickerViewModel.tickerSnapshot.observe(viewLifecycleOwner) { snapshot ->
             binding.volume.text = snapshot.volume.toString()
             binding.price.text = snapshot.lastPrice.toString()
             binding.lowPrice.text = snapshot.low.toString()
             binding.highPrice.text = snapshot.high.toString()
+        }
+    }
+
+    private fun setupBooks() {
+        binding.askRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.bidRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        binding.askRecyclerView.adapter = askAdapter
+        binding.bidRecyclerView.adapter = bidAdapter
+
+        tickerViewModel.askOrders.observe(viewLifecycleOwner) { orders ->
+            askAdapter.submitList(orders.map { BookItem.Ask(it) })
+        }
+        tickerViewModel.bidOrders.observe(viewLifecycleOwner) { orders ->
+            bidAdapter.submitList(orders.map { BookItem.Bid(it) })
         }
     }
 }
